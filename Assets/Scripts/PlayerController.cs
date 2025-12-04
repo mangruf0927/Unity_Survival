1d7977bdf38f2a0335f40d0f6a6a85f4f84d3265
mangruf <123349413+mangruf0927@users.noreply.github.com>
2025-12-04 20:15:53 +0900

    Feat: 마우스 회전에 따른 플레이어 이동 방향 적용

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1.0f;
     [SerializeField] private Rigidbody rigid;
+    
     private Vector2 moveDir;
+    private Vector2 mouseDelta;
+
+    void Update()
+    {
+        Look();
+    }
 
     void FixedUpdate()
     {
@@ -15,12 +22,25 @@ public class PlayerController : MonoBehaviour
     public void Move()
     {
         Vector3 moveVec = new Vector3(moveDir.x, 0f, moveDir.y);
-        rigid.linearVelocity = moveVec * moveSpeed;
+        Vector3 worldVec = transform.TransformDirection(moveVec);   // world space의 direction으로 변환
+
+        rigid.linearVelocity = worldVec * moveSpeed;
+    }
+
+    public void Look()
+    {
+        float mouseX = mouseDelta.x;
+        transform.Rotate(0f, mouseX, 0f);
     }
 
     // Input
     public void OnMove(InputValue value)
     {
         moveDir = value.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        mouseDelta = value.Get<Vector2>();
     }
 }
