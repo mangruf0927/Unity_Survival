bbeef6033f685f97dbb1bcac0d11cde0b2d0f1ce
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2026-02-20 14:37:23 +0900

    Feat : Object Pool 구현

---
+++ Assets/Scripts/New Folder/ObjectPool.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PoolTypeEnums { BULLET, ENEMY }
+
+public class ObjectPool : MonoBehaviour
+{    
+    private readonly List<Stack<GameObject>> poolList = new();
+
+    private void Awake() 
+    {
+        foreach (PoolTypeEnums type in Enum.GetValues(typeof(PoolTypeEnums)))
+        {
+            poolList.Add(new Stack<GameObject>());
+        }    
+    }
+
+    public void InitializePool(int poolSize, GameObject prefab, PoolTypeEnums poolType, Transform parent = null)
+    {
+        var pool = poolList[(int)poolType];
+
+        for(int i = 0; i < poolSize; i++)
+        {
+            pool.Push(CreatePool(prefab, parent));
+        }
+    }
+
+    public GameObject GetFromPool(GameObject prefab, PoolTypeEnums poolType, Transform parent = null)
+    {
+        var pool = poolList[(int)poolType];
+
+        GameObject obj = pool.Count > 0 ? pool.Pop() : CreatePool(prefab, parent);
+        obj.SetActive(true);
+        return obj;
+    }
+
+    private GameObject CreatePool(GameObject prefab, Transform parent = null)
+    {
+        GameObject obj = Instantiate(prefab, parent);
+        obj.SetActive(false);
+        return obj;
+    }
+
+    public void ReturnToPool(GameObject obj, PoolTypeEnums poolType)
+    {
+        obj.SetActive(false);
+        poolList[(int)poolType].Push(obj);
+    }
+}
