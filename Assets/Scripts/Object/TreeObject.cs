2813b011a2e90a0cfa9554c400de4ca10e426a5b
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2026-03-13 17:44:55 +0900

    Feat : 나무 파괴 시 통나무 생성

--- Assets/Scripts/Object/TreeObject.cs
+++ Assets/Scripts/Object/TreeObject.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class TreeObject : MonoBehaviour
 {
+    [SerializeField] private GameObject log;
     [SerializeField] private int maxHP = 100;
     [SerializeField] private float shakeAngle;
     [SerializeField] private float shakeTime;
@@ -24,7 +25,12 @@ public class TreeObject : MonoBehaviour
         currentHP = Mathf.Max(currentHP - damage, 0);
 
         if (!isShaking) StartCoroutine(Shake());
-        if (currentHP == 0) Destroy(gameObject);
+        if (currentHP == 0) 
+        {
+            StopAllCoroutines();
+            CreateLog();
+            Destroy(gameObject);
+        }
     }
 
     private IEnumerator Shake()
@@ -34,8 +40,9 @@ public class TreeObject : MonoBehaviour
         Quaternion left = original * Quaternion.Euler(0f, 0f, shakeAngle);
         Quaternion right = original * Quaternion.Euler(0f, 0f, -shakeAngle);
 
-        yield return Rotate(original, left, shakeTime * 0.5f);
-        yield return Rotate(left, right, shakeTime * 0.5f);
+        yield return Rotate(original, left, shakeTime * 0.33f);
+        yield return Rotate(left, right, shakeTime * 0.34f);
+        yield return Rotate(right, original, shakeTime * 0.33f);
 
         transform.rotation = original;
         isShaking = false;
@@ -44,11 +51,20 @@ public class TreeObject : MonoBehaviour
     private IEnumerator Rotate(Quaternion from, Quaternion to, float duration)
     {
         float time = 0;
-        while(time < duration)
+        while (time < duration)
         {
             time += Time.deltaTime;
             transform.rotation = Quaternion.Slerp(from, to, time / duration);
             yield return null;
+        }
+    }
+
+    private void CreateLog()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-0.5f, 0.5f), 0.3f, Random.Range(-0.5f, 0.5f));
+            Instantiate(log, transform.position + randomPos, Quaternion.identity);
         }
     }
 }
