c8ed273456b672d517fd336fa68fcd8abb7bd100
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2026-04-24 16:21:32 +0900

    Feat : 캠프파이어 연료 감소 구현

--- Assets/Scripts/Object/CampFire.cs
+++ Assets/Scripts/Object/CampFire.cs
@@ -1,37 +1,94 @@
+using System.Collections;
 using UnityEngine;
 
 public class CampFire : MonoBehaviour
 {
     [SerializeField] private int maxLevel = 6;
-    [SerializeField] private int[] fuelLevelList = { 1, 6, 20, 40, 80, 140 };
+    [SerializeField] private ParticleSystem fire;
+
+    [SerializeField] private int[] fuelLevelList;
+    [SerializeField] private float intervalTime;
+    [SerializeField] private int decreaseAmount;
 
     private int currentLevel = 0;
     private int currentFuel = 0;
 
     public int CurrentLevel => currentLevel;
     public int CurrentFuel => currentFuel;
-    public bool IsLit => currentFuel > 0;
+    private Coroutine decreaseCoroutine;
+
+    private void Start()
+    {
+        OffFire();
+    }
 
     private void AddFuel(int amount)
     {
         if (amount <= 0) return;
+
         currentFuel += amount;
-        if (currentFuel >= fuelLevelList[currentLevel]) LevelUp();
+        if (CanLevelUp()) LevelUp();
+
+        OnFire();
+        StartDecreaseFuel();
+    }
+
+    private void StartDecreaseFuel()
+    {
+        if (decreaseCoroutine != null) return;
+
+        decreaseCoroutine = StartCoroutine(DecreaseFuel());
+    }
+
+    private IEnumerator DecreaseFuel()
+    {
+        while (currentFuel > 0)
+        {
+            yield return new WaitForSeconds(intervalTime);
+            currentFuel -= decreaseAmount;
+            Debug.Log(currentFuel);
+
+            if (currentFuel <= 0)
+            {
+                currentFuel = 0;
+                OffFire();
+                break;
+            }
+        }
+        decreaseCoroutine = null;
+    }
+
+    private bool CanLevelUp()
+    {
+        if (currentLevel >= maxLevel) return false;
+        if (currentLevel >= fuelLevelList.Length) return false;
+        if (currentFuel < fuelLevelList[currentLevel]) return false;
+
+        return true;
     }
 
     private void LevelUp()
     {
-        if (currentLevel >= maxLevel) return;
         currentLevel += 1;
+    }
+
+    private void OnFire()
+    {
+        if (!fire.isPlaying) fire.Play();
+    }
+
+    private void OffFire()
+    {
+        fire.Stop(true, ParticleSystemStopBehavior.StopEmitting);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Item item = other.GetComponent<Item>();
         if (item == null) return;
-        // if (item.ItemType != ItemType.FUEL) return;
+        if (item.Data.ItemType != ItemType.FUEL) return;
 
-        // AddFuel(item.Value);
+        AddFuel(item.Data.FuelData.BurnPower);
         Destroy(other.gameObject);
     }
 }
