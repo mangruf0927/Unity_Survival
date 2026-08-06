6172d7a1105292911b707b7d7815694abc678696
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2026-08-06 16:22:36 +0900

    Update : enemy 추적 경로 갱신 주기 수정

--- Assets/Scripts/Enemy/EnemyStates/EnemyChaseState.cs
+++ Assets/Scripts/Enemy/EnemyStates/EnemyChaseState.cs
@@ -1,7 +1,12 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
 public class EnemyChaseState : IEnemyState
 {
     private readonly EnemyController enemyController;
     private readonly EnemyStateMachine stateMachine;
+
+    private CancellationTokenSource cts;
 
     public EnemyChaseState(EnemyStateMachine _stateMachine, EnemyController _enemyController)
     {
@@ -12,15 +17,25 @@ public class EnemyChaseState : IEnemyState
     public void Enter()
     {
         enemyController.Animator.SetFloat("speed", 2f);
+        cts = CancellationTokenSource.CreateLinkedTokenSource(stateMachine.destroyCancellationToken);
+        RepathAsync(cts.Token).Forget();
+    }
+
+    private async UniTask RepathAsync(CancellationToken ct)
+    {
+        while (!ct.IsCancellationRequested)
+        {
+            enemyController.Chase();
+
+            bool canceled = await UniTask.Delay(200, cancellationToken: ct).SuppressCancellationThrow();
+
+            if (canceled) return;
+        }
     }
 
     public void Update()
     {
-        if (enemyController.ShouldChasePlayer())
-        {
-            enemyController.Chase();
-        }
-        else
+        if (!enemyController.ShouldChasePlayer())
         {
             stateMachine.ChangeState(EnemyStateEnums.IDLE);
         }
@@ -28,11 +43,12 @@ public class EnemyChaseState : IEnemyState
 
     public void FixedUpdate()
     {
-
     }
 
     public void Exit()
     {
-
+        cts?.Cancel();
+        cts?.Dispose();
+        cts = null;
     }
 }
